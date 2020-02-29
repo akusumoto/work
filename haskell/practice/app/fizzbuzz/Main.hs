eb2a9fe84245bb2f17cedb50178293748cb4e5db
Akira Kusumoto <gkusumoto@gmail.com>
2020-02-29 19:18:33 +0900

    fizzbuzz -> practice (project)

--- haskell/practice/app/fizzbuzz/Main.hs
+++ haskell/practice/app/fizzbuzz/Main.hs
@@ -1,7 +1,11 @@
+module Main where
+
 g 0 0 x="fizzbuzz"
 g 0 _ x="fizz"
 g _ 0 x="buzz"
 g _ _ x=show x
 f[]=""
 f(x:y)=g(mod x 3)(mod x 5)x++"\n"++f y
-main=putStr$f[1..100]
+
+main :: IO()
+main = putStr$f[1..100]
