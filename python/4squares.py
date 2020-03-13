5cc09eae8795b04cb42521d6815cd0d64f640022
Akira Kusumoto <akira.kusumoto@hennge.com>
2020-03-13 09:29:16 +0900

    4squares

--- python/4squares.py
+++ python/4squares.py
@@ -1,5 +1,6 @@
 # https://rosettacode.org/wiki/4-rings_or_4-squares_puzzle
 def c(a,z):
- if len(a)>0:for n in a:c([j for j in a if j!=n],z+[n])
- else:if sum(z[:2])==sum(z[1:4])==sum(z[3:6])==sum(z[5:]):print z
+ f=1
+ for n in a:c([j for j in a if j!=n],z+[n]);f=0
+ if f and sum(z[:2])==sum(z[1:4])==sum(z[3:6])==sum(z[5:]):print z
 c([i for i in range(1,8)],[])
